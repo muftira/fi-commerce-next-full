66ac960e9f24dbf38a756eaef6d4f21cddc25b58
muftira <muh.muftir@gmail.com>
2025-11-05 05:03:38 +0700

    added middleware in route products-by-userid

--- src/pages/api/products-by-userid.ts
+++ src/pages/api/products-by-userid.ts
@@ -2,13 +2,10 @@ import { getAllProductsbyUserId } from '@/controller/productController';
 import { createRouter } from 'next-connect';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { adminRole, authentication } from '@/middlewares/authentication';
-import { adaptMiddleware } from '@/middlewares/adaptMiddleware';
-import upload from '@/middlewares/multer';
-import { get } from 'http';
 
 const router = createRouter<NextApiRequest, NextApiResponse>();
 
-router.get(getAllProductsbyUserId);
+router.get(authentication, adminRole, getAllProductsbyUserId);
 
 export const config = {
     api: {
