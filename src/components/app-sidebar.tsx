dd79d082f17078c6d8b8b4b032f4607122ce3336
muftira <muh.muftir@gmail.com>
2025-11-05 04:06:42 +0700

    feature/fic-4:fixed wording fi commerce

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -117,7 +117,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
                   <Command className="size-4" />
                 </div>
                 <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-semibold">FI Comerce</span>
+                  <span className="truncate font-semibold">FI Commerce</span>
                   <span className="truncate text-xs">Dashboard</span>
                 </div>
               </a>
